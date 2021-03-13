509ed92174b1dc849d4091e772526b210f5fe8c8
Marika <mashakonturova@gmail.com>
2021-03-13 07:42:09 +0300

    localstorage useEffect change

--- src/Setter/Display-setter/Display-setter.tsx
+++ src/Setter/Display-setter/Display-setter.tsx
@@ -1,14 +1,17 @@
 import React, {ChangeEventHandler, useState} from "react";
 import st from './Display-setter.module.css'
+
 type DisplaySetterPropsType = {
-    onNewSetterValue:(e:any)=>void
-    value:number
-    children:string
+    onNewSetterValue: (e: any) => void
+    value: number
+    children: string
 
 }
-export function DisplaySetter(props:DisplaySetterPropsType) {
+
+export function DisplaySetter(props: DisplaySetterPropsType) {
 
     return (<div className={`${st.body} ${st.template_4up}`}>
-        <label>{props.children}</label> <input type="number"  id="number"  value={props.value} onChange={props.onNewSetterValue} className={st.input}/>
+        <label>{props.children}</label>
+        <input type="number" id="number" value={props.value} onChange={props.onNewSetterValue} className={st.input}/>
     </div>)
 }
--- src/Setter/Setter.tsx
+++ src/Setter/Setter.tsx
@@ -2,37 +2,54 @@ import React, {ChangeEvent, EffectCallback, useEffect, useState} from "react";
 import st from "./Setter.module.css";
 import {Button} from "../Button/Button";
 import {DisplaySetter} from "./Display-setter/Display-setter";
-type SetterPropsType ={
-    getValue:()=>void
+
+type SetterPropsType = {
+    getValue: () => void
 }
-export function Setter(props:SetterPropsType) {
+
+export function Setter(props: SetterPropsType) {
     const [minSetValue, setMinSetValue] = useState(0)
     const [maxSetValue, setMaxSetValue] = useState(0)
 
-    const onNewSetterMinValue=(e:ChangeEvent<HTMLInputElement>)=>{
+    const onNewSetterMinValue = (e: ChangeEvent<HTMLInputElement>) => {
+        localStorage.setItem('minValue', JSON.stringify(minSetValue + 1))
         setMinSetValue(e.currentTarget.valueAsNumber);
     }
-    const onNewSetterMaxValue=(e:ChangeEvent<HTMLInputElement>)=>{
+    const onNewSetterMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
+        localStorage.setItem('maxValue', JSON.stringify(maxSetValue+ 1))
         setMaxSetValue(e.currentTarget.valueAsNumber);
     }
-    useEffect(()=>{
-        localStorage.setItem('minValue', JSON.stringify(minSetValue))
-    },[minSetValue])
-    useEffect(()=>{
-        localStorage.setItem('maxValue', JSON.stringify(maxSetValue))
-    },[maxSetValue])
-useEffect(()=>{
-    const newMaxValue = localStorage.getItem('maxValue')
-    const newMinValue = localStorage.getItem('minValue')
-    setMaxSetValue(JSON.parse(newMaxValue ? newMaxValue : '5'))
-    setMinSetValue(JSON.parse(newMinValue ? newMinValue : '0'))
 
-},[])
+    useEffect(() => {
+        const newMaxValue = localStorage.getItem('maxValue')
+        const newMinValue = localStorage.getItem('minValue')
+        if (newMinValue) {
+            setMinSetValue(JSON.parse(newMinValue))
+        }
+        if (newMaxValue) {
+            setMaxSetValue(JSON.parse(newMaxValue))
+        }/*
+
+    setMaxSetValue(JSON.parse(newMaxValue ? newMaxValue : '5'))
+    setMinSetValue(JSON.parse(newMinValue ? newMinValue : '0'))*/
+
+    }, [])
+    //
+    // useEffect(() => {
+    //     localStorage.setItem('minValue', JSON.stringify(minSetValue))
+    // }, [minSetValue])
+    //
+    // useEffect(() => {
+    //     localStorage.setItem('maxValue', JSON.stringify(maxSetValue))
+    // }, [maxSetValue])
+
+
 
     return (
-        <div className={st.counter}><div className={st.display}>
-            <DisplaySetter onNewSetterValue={onNewSetterMaxValue} value={maxSetValue} children={'maxValue'}/>
-            <DisplaySetter onNewSetterValue={onNewSetterMinValue} value={minSetValue} children={'minValue'}/></div>
+        <div className={st.counter}>
+            <div className={st.display}>
+                <DisplaySetter onNewSetterValue={onNewSetterMaxValue} value={maxSetValue} children={'maxValue'}/>
+                <DisplaySetter onNewSetterValue={onNewSetterMinValue} value={minSetValue} children={'minValue'}/></div>
             <Button onClick={props.getValue} disabled={minSetValue >= maxSetValue} children={'set'}/>
         </div>
     )
